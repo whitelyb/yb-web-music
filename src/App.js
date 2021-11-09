5520fbe5bbb1435b9637d694a0f4e055bfbdf129
liaoyangbiao <2586224025@qq.com>
2021-11-09 17:48:52 +0800

    测试

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default memo(function App() {
     return (
         <HashRouter>
             <YBAppHeader />
-            { renderRoutes(routes) }
+            { renderRoutes(routes) } 
             <YBAppFooter />
         </HashRouter>
     )

fba8b7aee9c8cf9582b25c82faa55f4d9b850842
liaoyangbiao <2586224025@qq.com>
2021-11-10 17:23:37 +0800

    添加顶部轮播图

--- src/pages/discover/c-pages/recommend/c-pages/top-banner/index.js
+++ src/pages/discover/c-pages/recommend/c-pages/top-banner/index.js
@@ -1,8 +1,9 @@
-import React, { memo, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { memo, useEffect, useRef, useState, useCallback } from 'react'
+import { useDispatch, useSelector, shallowEqual } from 'react-redux'
 
 import { getBanner } from '../../store/actionCreators';
 
+import { Carousel } from "antd";
 import {
     BannerWrapper,
     BannerLeft,
@@ -11,21 +12,49 @@ import {
 } from './style';
 
 export default memo(function YBTopBanner() {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const dispatch = useDispatch();
+    const { banners } = useSelector(state => ({
+        banners: state.getIn(["recommend", "topBanners"])
+    }), shallowEqual)
 
+    const bannerRef = useRef();
     useEffect(() => {
         dispatch(getBanner());
     }, [dispatch])
 
+    const bannerChange = useCallback((from, to) => {
+        setTimeout(() => {
+            setCurrentIndex(from);
+        }, 0)
+    },[])
+
+    // 获取轮播图背景图片
+    const bgImage = banners[currentIndex] && (banners[currentIndex].imageUrl + "?imageView&blur=40x20");
+
     return (
-        <BannerWrapper>
-            <BannerLeft></BannerLeft>
-            <BannerRight></BannerRight>
-            <BannerControl>
-                <div className="btn left"></div>
-                <div className="btn right"></div>
-            </BannerControl>
+        <BannerWrapper bgImage={bgImage}>
+            <div className="banner wrap-v2">
+                <BannerLeft>
+                    <Carousel effect="fade" beforeChange={bannerChange} ref={bannerRef} dots={{className: "dot-class"}}>
+                        {
+                            banners.map(item => {
+                                return (
+                                    <div className="banner-item" key={item.imageUrl}>
+                                        <img className="image" src={item.imageUrl} alt={item.typeTitle} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </Carousel>
+                </BannerLeft>
+                <BannerRight></BannerRight>
+                <BannerControl className="control">
+                    <div className="btn left" onClick={e => bannerRef.current.prev()}></div>
+                    <div className="btn right" onClick={e => bannerRef.current.next()}></div>
+                </BannerControl>
+            </div>
         </BannerWrapper>
     )
 })
--- src/pages/discover/c-pages/recommend/c-pages/top-banner/style.js
+++ src/pages/discover/c-pages/recommend/c-pages/top-banner/style.js
@@ -1,18 +1,63 @@
 import styled from 'styled-components';
 
+import downloadImg from "@/assets/img/download.png";
+import banner_sprite from "@/assets/img/banner_sprite.png";
+
 export const BannerWrapper = styled.div`
     background: url(${props => props.bgImage}) center center/6000px;
 
     .banner {
         height: 270px;
-        background-color: red;
-
         display: flex;
         position: relative;
     }
 `
 
 export const BannerLeft = styled.div`
+    .dot-class {
+        li {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 20px;
+            height: 20px;
+            margin: 0 3px;
+            cursor: pointer;
+            button {
+                position: relative;
+                width: 100%;
+                height: 100%;
+                border-radius: 50%;
+                background: transparent;
+                opacity: .7;
+                &::after {
+                    content: "";
+                    position: absolute;
+                    width: 6px;
+                    height: 6px;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    background: #fff;
+                    border-radius: 50%;
+                }
+            }
+            &:hover button::after {
+                background: red;
+            }
+            
+        }
+        .slick-active {
+            width: 20px!important;
+            button {
+                background: transparent!important;
+                &::after {
+                    background: red;
+                }
+            }
+        }
+    }
+
     width: 730px;
 
     .banner-item {
@@ -30,21 +75,17 @@ export const BannerRight = styled.a.attrs({
 })`
     width: 254px;
     height: 270px;
-    background: url(${require("@/assets/img/download.png")});
+    background: url(${downloadImg});
 `
 
 export const BannerControl = styled.div`
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 50%;
-    transform: translateY(-50%);
-
     .btn {
         position: absolute;
         width: 37px;
         height: 63px;
-        background-image: url(${require("@/assets/img/banner_sprite.png")});
+        top: 50%;
+        transform: translateY(-50%);
+        background-image: url(${banner_sprite});
         background-color: transparent;
         cursor: pointer;
 
